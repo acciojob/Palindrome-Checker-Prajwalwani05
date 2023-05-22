35985e9a7ff239f069c6a07c40a71c2f1d55c022
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-22 15:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+	var s =	str.toLowerCase()
+var i = 0
+	var j = s.length - 1;
+	while (i<j) {
+		if(s.charAt(i)===s.charAt(j)) {
+			i++;
+			j--;
+		}
+		else {
+			return false
+		}
+		
+	}
+	return true
 
 }
 module.exports = palindrome
